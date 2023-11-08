ca45ec2032bd769085a391bb32e1d3109658bfb0
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-11-08 22:29:27 +0800

    Add exceptions

---
+++ HRIS/HRIS.Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace HRIS.Domain.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException() { }
+
+        public ValidationException(string? message) : base(message) { }
+
+        public ValidationException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
